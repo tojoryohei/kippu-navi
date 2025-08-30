c4a4552eb1e44502938cdb376791877177fc51fb
tojoryohei <tojoryohei@yahoo.co.jp>
2025-08-31 02:39:52 +0900

    renew Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="border-t flex justify-center items-center h-14">
-            <div>Copyright © 2025 TOJO Ryohei. All Rights Reserved.</div>
+            <div>Copyright © 2025 きっぷナビ. All Rights Reserved.</div>
         </footer>
     );
 };
