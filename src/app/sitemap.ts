a70904717784c96fdde1a9ef58eaf2462525a317
tojoryohei <tojoryohei@yahoo.co.jp>
2025-10-14 02:27:33 +0900

    feat: robot.txtとサイトマップの作成

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = 'https://kippu-navi.com';
+
+    const staticUrls = [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/mr`,
+            lastModified: new Date(),
+        },
+    ];
+
+    return [...staticUrls];
+}
