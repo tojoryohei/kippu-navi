688825d8a86ebacf840cfcf2b800382fd8608603
tojoryohei <tojoryohei@yahoo.co.jp>
2025-10-20 16:12:53 +0900

    feat: プライバシーポリシーの制定

---
+++ src/app/guide/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "利用案内",
+};
+
+export default function GuideLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <>{children}</>;
+}
