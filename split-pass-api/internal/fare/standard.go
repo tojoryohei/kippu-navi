18e85a6a21980272cbf43054b97d21db53429db3
tojoryohei <106292045+tojoryohei@users.noreply.github.com>
2026-05-26 18:32:50 +0900

    feat(fare): JR東海・西日本の Calculator を実装

---
+++ split-pass-api/internal/fare/standard.go
@@ -0,0 +1,34 @@
+package fare
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+)
+
+//go:embed data/standardTrunkFares.json
+var standardTrunkFaresJSON []byte
+
+//go:embed data/standardLocalFares.json
+var standardLocalFaresJSON []byte
+
+func NewStandardCalculator() (*StandardCalculator, error) {
+	var standardTrunkFares [101]PassFare
+	if err := json.Unmarshal(standardTrunkFaresJSON, &standardTrunkFares); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal standardTrunkFares: %w", err)
+	}
+	var standardLocalFares [101]PassFare
+	if err := json.Unmarshal(standardLocalFaresJSON, &standardLocalFares); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal standardLocalFares: %w", err)
+	}
+	return &StandardCalculator{trunkFares: standardTrunkFares, localFares: standardLocalFares}, nil
+}
+
+type StandardCalculator struct {
+	trunkFares [101]PassFare
+	localFares [101]PassFare
+}
+
+func (c *StandardCalculator) Calculate(params PassFareParams) (int, error) {
+	return calculateBaseFare(params, &c.trunkFares, &c.localFares)
+}
