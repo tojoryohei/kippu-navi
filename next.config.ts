02433ee5b02a8ae5f338dca4b35682d7d63c59b6
tojoryohei <106292045+tojoryohei@users.noreply.github.com>
2026-06-18 21:58:52 +0900

    feat: posthog-jsのインストールとnext.config.tsへのリバースプロキシ設定の追加

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,22 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
   async redirects() {
     return [
       {
