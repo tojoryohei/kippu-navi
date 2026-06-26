0c9a825913402fef6615511981098bbcfac17c42
tojoryohei <106292045+tojoryohei@users.noreply.github.com>
2026-06-26 20:18:20 +0900

    ci: dummy.go を削除し、GitHub Actions 側で cmd/wasm をテスト対象から除外するように修正

--- split-pass-api/cmd/wasm/dummy.go
+++
@@ -1,8 +0,0 @@
-//go:build !js || !wasm
-
-package main
-
-// このファイルは、js/wasm以外のプラットフォームで go test ./... を実行した際に、
-// cmd/wasm パッケージ内にビルド可能なGoファイルが1つも存在しないために発生する
-// [setup failed] エラーを回避するためのダミー実装です。
-func main() {}
