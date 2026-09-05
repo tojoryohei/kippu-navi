9938f2229fd2644e9c20282bc9df3663e8f0eaf3
tojoryohei <tojoryohei@yahoo.co.jp>
2026-09-05 12:25:30 +0900

    fix: 静的エクスポート時のビルドエラーを修正
    - tsconfig.jsonでfunctionsディレクトリを型チェックの対象外に設定
    
    - robots.ts, sitemap.ts を強制的に静的エクスポートするよう export const dynamic = 'force-static' を追加

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,5 @@
 import { MetadataRoute } from 'next'
+export const dynamic = 'force-static';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://kippu-navi.com';
